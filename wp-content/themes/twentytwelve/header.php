a22c9edc6dc562a2ecc8241a06453f1492b0d58a
Linden Melvin <linden.melvin@gmail.com>
2014-09-15 20:13:31 -0700

    styling the ads

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -41,7 +41,7 @@
 		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<!-- First Ad Unit -->
 		<ins class="adsbygoogle"
-		    style="display:inline-block;width:728px;height:90px;float:right;"
+		    style="display:inline-block;width:728px;height:90px;"
 		    data-ad-client="ca-pub-1727917029633384"
 		    data-ad-slot="1053185751"></ins>
 		<script>
--- wp-content/themes/twentytwelve/sidebar.php
+++ wp-content/themes/twentytwelve/sidebar.php
@@ -12,12 +12,9 @@
 
 	<!-- Side Nav -->
 	<ins class="adsbygoogle"
-	     style="display:inline-block;width:300px;height:600px"
+	     style="display:inline-block;width:300px;height:600px;margin:10px;"
 	     data-ad-client="ca-pub-1727917029633384"
 	     data-ad-slot="9450436551"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
