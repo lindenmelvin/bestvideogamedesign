97967b3f6a68f3bfefc0806af3500186d298d1dd
Linden Melvin <linden.melvin@gmail.com>
2014-09-15 20:09:01 -0700

    adding right side ad

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -40,7 +40,7 @@
 		</hgroup>
 		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<!-- First Ad Unit -->
-		<ins class="adsbygoogle"
+		<ins style="float:right" class="adsbygoogle"
 		    style="display:inline-block;width:728px;height:90px"
 		    data-ad-client="ca-pub-1727917029633384"
 		    data-ad-slot="1053185751"></ins>
